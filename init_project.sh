e40d6cc11ee98cf1ca55212bc736d710e1242c7e
Jason Kimko <22348009+jskimko@users.noreply.github.com>
2023-11-02 02:52:55 -0700

    Fix init_project.sh

--- init_project.sh
+++ init_project.sh
@@ -23,23 +23,24 @@ if [ -d '.git' ]; then
   git=git
 fi
 
-for f in $(grep -irl xxx --exclude-dir='build' --exclude-dir='.*'); do
+for f in $(grep -irl example --exclude-dir='build' --exclude-dir='.*'
+         | grep -v $(basename $0)); do
   bname=$(basename "$f")
   if echo "$bname" | grep -Eq '^\.'; then
     continue
   fi
 
   echo "Updating $bname"
-  sed -i "s/xxx/$lower/g" "$f"
-  sed -i "s/XXX/$upper/g" "$f"
+  sed -i "s/example/$lower/g" "$f"
+  sed -i "s/EXAMPLE/$upper/g" "$f"
 done
 
-for f in $(find . -type f -iname '*xxx*' -not -path './build*'); do
-  $git mv $f $(echo $f | sed "s:/\([^/]*\)xxx\([^/]*\)$:/\1${lower}\2:")
+for f in $(find . -type f -iname '*example*' -not -path './build*'); do
+  $git mv $f $(echo $f | sed "s:/\([^/]*\)example\([^/]*\)$:/\1${lower}\2:")
 done
 
-for f in $(find . -type d -iname '*xxx*' -not -path './build*'); do
-  $git mv $f $(echo $f | sed "s:xxx:${lower}:")
+for f in $(find . -type d -iname '*example*' -not -path './build*'); do
+  $git mv $f $(echo $f | sed "s:example:${lower}:")
 done
 
 echo
