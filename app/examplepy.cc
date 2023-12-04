e4af4ddba4c8d20f5bb974552e1000d80245b195
Jason Kimko <22348009+jskimko@users.noreply.github.com>
2023-12-04 08:54:36 -0800

    Add py usage

--- app/examplepy.cc
+++ app/examplepy.cc
@@ -1,12 +1,18 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
-#include <example/config.hh>
+
+#include <example/example.hh>
+#include <cstring>
 
 #ifdef EXAMPLE_ENABLE_MPI
 #include <mpi.h>
-#define EXIT(rc) MPI_Abort(MPI_COMM_WORLD, rc);
+#define EXIT(rc) do { MPI_Finalize(); std::exit(rc); } while (0)
+#define PRINTF(...) do { if (rank == 0) printf(__VA_ARGS__); } while (0)
+#define FPRINTF(f, ...) do { if (rank == 0) fprintf(f, __VA_ARGS__); } while (0)
 #else
 #define EXIT(rc) std::exit(rc);
+#define PRINTF(...) do { printf(__VA_ARGS__); } while (0)
+#define FPRINTF(f, ...) do { fprintf(f, __VA_ARGS__); } while (0)
 #endif
 
 extern "C" {
@@ -24,7 +30,25 @@ int main(int argc, char *argv[]) {
 
 #ifdef EXAMPLE_ENABLE_MPI
   MPI_Init(&argc, &argv);
+
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 #endif
+
+  auto usage = [&argv]() {
+    return fmt::format(
+      "{0} - convenience program to enable easy `import {0}`\n"
+      "usage: {0} [PYTHON_ARGS...] script.py [SCRIPT_ARGS...]",
+      EXAMPLE_PROJECT_NAME);
+  };
+
+  for (int i=0; i<argc; i++) {
+    if (std::strcmp(argv[i], "-h") == 0 || std::strcmp(argv[i], "--help") == 0) {
+      PRINTF("%s\n", usage().c_str());
+      EXIT(0);
+    }
+  }
   
   // BEGIN https://docs.python.org/3.10/c-api/init_config.html
   PyStatus status;
@@ -33,7 +57,7 @@ int main(int argc, char *argv[]) {
   auto exception = [&]() {
     PyConfig_Clear(&config);
     if (!PyStatus_IsExit(status)) {
-      fprintf(stderr, "pyinit error: %s\n", status.err_msg);
+      FPRINTF(stderr, "pyinit error: %s\n", status.err_msg);
     }
     EXIT(status.exitcode);
   };
